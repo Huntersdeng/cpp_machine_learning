876ffac654cbd8e76c4ae2490b15f068b662dc18
Huntersdeng <1050130028@qq.com>
2022-08-13 13:20:16 +0800

    update test.cpp for mnist dataset

--- chapter3/test.cpp
+++ chapter3/test.cpp
@@ -18,35 +18,59 @@ using Eigen::Matrix2i;
 using Eigen::VectorXf;
 
 
-int main() {
-    MatrixXf X;
-    VectorXf y;
-    read_csv2matrix("./watermelon.csv", X, y);
-    int m = X.rows();
-    int d = X.cols();
+int main(int argc, char* argv[]) {
+    int label0 = 0, label1 = 1;
+    if(argc >= 3) {
+        label0 = atoi(argv[1]);
+        label1 = atoi(argv[2]);
+    }
+    MatrixXf X_train, X_test;
+    VectorXf y_train, y_test;
+    // read_csv2matrix("./watermelon.csv", X, y);
+    vector<int> idx_train, idx_test;
+    read_Mnist_Label("./dataset/mnist/train-labels.idx1-ubyte", y_train, label0, label1, idx_train);
+    read_Mnist_Label("./dataset/mnist/t10k-labels.idx1-ubyte", y_test, label0, label1, idx_test);
+    read_Mnist_Images("./dataset/mnist/train-images.idx3-ubyte", X_train, idx_train);
+    read_Mnist_Images("./dataset/mnist/t10k-images.idx3-ubyte", X_test, idx_test);
+    
+    int m = X_train.rows();
+    int m_test = X_test.rows();
+    int d = X_train.cols();
+    int d_test = X_test.cols();
+    std::cout << m << " " << d << " " << m_test << " " << d_test << std::endl;
 
     /* logistric regression */
-    // LogisticReg lr(d);
-    // lr.solve(X, y, 20);
+    X_train.conservativeResize(m, d + 1);
+    X_train.col(d) = VectorXf::Ones(m);
+    X_test.conservativeResize(m_test, d + 1);
+    X_test.col(d) = VectorXf::Ones(m_test);
+    LogisticReg lr(d);
+    lr.solve(X_train, y_train, 10);
     // std::cout << lr.get_w().transpose() << std::endl;
-    // VectorXf pred = lr.predict(X);
+    VectorXf pred = lr.predict(X_test);
+    for(int i=0; i<m_test; ++i) {
+        if(pred(i) >= 0.5) {
+            pred(i) = 1;
+        } else {
+            pred(i) = 0;
+        }
+    }
 
     /* LDA */
-    MatrixXf X_data = X.block(0, 0, m, d-1);
-    LDA lda(d-1);
-    lda.solve(X_data, y);
-    VectorXf pred = lda.predict(X_data);
+    // LDA lda(d);
+    // lda.solve(X, y);
+    // VectorXf pred = lda.predict(X);
 
     /* Result */
     Matrix2i mat;
-    Metric::confusion_matrix(pred, y, mat);
-    std::cout << "labels:      " << y.transpose() << std::endl;
-    std::cout << "Predictions: " << pred.transpose() << std::endl;
-    std::cout << "MSE: " << Metric::mse(pred, y) << std::endl;
-    std::cout << "Accuracy: " << Metric::accuracy(pred, y) << std::endl;
-    std::cout << "Error_rate: " << Metric::error_rate(pred, y) << std::endl;
+    Metric::confusion_matrix(pred, y_test, mat);
+    // std::cout << "labels:      " << y_test.transpose() << std::endl;
+    // std::cout << "Predictions: " << pred.transpose() << std::endl;
+    std::cout << "MSE: " << Metric::mse(pred, y_test) << std::endl;
+    std::cout << "Accuracy: " << Metric::accuracy(pred, y_test) << std::endl;
+    std::cout << "Error_rate: " << Metric::error_rate(pred, y_test) << std::endl;
     std::cout << "Confusion matrix:\n" << mat << std::endl;
-    std::cout << "Precision: " << Metric::precision(pred, y) << std::endl;
-    std::cout << "Recall: " << Metric::recall(pred, y) << std::endl;
-    std::cout << "F1_score: " << Metric::f1_score(pred, y) << std::endl;
+    std::cout << "Precision: " << Metric::precision(pred, y_test) << std::endl;
+    std::cout << "Recall: " << Metric::recall(pred, y_test) << std::endl;
+    std::cout << "F1_score: " << Metric::f1_score(pred, y_test) << std::endl;
 }
--- utils/dataset.h
+++ utils/dataset.h
@@ -4,9 +4,13 @@
 #include <fstream>
 #include <sstream>
 #include <Eigen/Core>
+#include <Eigen/Dense>
 
 using std::vector;
 using std::string;
+using std::cout;
+using std::endl;
+using std::fstream;
 using Eigen::MatrixXf;
 using Eigen::VectorXf;
 
@@ -36,13 +40,12 @@ void read_csv2matrix(const string &path, MatrixXf &X, VectorXf &y, bool verpose=
     vector<int> y_data;
     read_csv2vector(path, X_data, y_data);
     int m = X_data.size(), d = X_data[0].size();
-    X.resize(m, d+1);
+    X.resize(m, d);
     y.resize(m, 1);
     for(int i=0; i<m; ++i) {
         for(int j=0; j<d+1; ++j) {
             if(j == d) {
                 y(i, 0) = y_data[i];
-                X(i, j) = 1;
             } else {
                 X(i, j) = X_data[i][j];
             }
@@ -50,6 +53,116 @@ void read_csv2matrix(const string &path, MatrixXf &X, VectorXf &y, bool verpose=
     }
     if(verpose) {
         std::cout << X << std::endl;
-        std::cout << y << std::endl;
+        std::cout << y.transpose() << std::endl;
     }
 }
+
+int ReverseInt(int i)
+{
+	unsigned char ch1, ch2, ch3, ch4;
+	ch1 = i & 255;
+	ch2 = (i >> 8) & 255;
+	ch3 = (i >> 16) & 255;
+	ch4 = (i >> 24) & 255;
+	return((int)ch1 << 24) + ((int)ch2 << 16) + ((int)ch3 << 8) + ch4;
+}
+ 
+void read_Mnist_Label(string filename, VectorXf&labels, int label0, int label1, vector<int> &idx) {
+	std::ifstream file(filename, std::ios::binary);
+	if (file.is_open())
+	{
+		int magic_number = 0;
+		int number_of_images = 0;
+		file.read((char*)&magic_number, sizeof(magic_number));
+		file.read((char*)&number_of_images, sizeof(number_of_images));
+		magic_number = ReverseInt(magic_number);
+		number_of_images = ReverseInt(number_of_images);
+		cout << "magic number = " << magic_number << endl;
+		cout << "number of images = " << number_of_images << endl;
+
+        vector<int> vec_labels;
+        int n = 0;
+		for (int i = 0; i < number_of_images; i++)
+		{
+			unsigned char label = 0;
+			file.read((char*)&label, sizeof(label));
+            if((float)label == label0) {
+                vec_labels.push_back(0);
+                idx.push_back(i);
+                ++n;
+            } else if((float)label == label1) {
+                vec_labels.push_back(1);
+                idx.push_back(i);
+                ++n;
+            }
+		}
+
+        labels.resize(vec_labels.size());
+        for(int i=0; i<vec_labels.size(); ++i) {
+            labels(i) = vec_labels[i];
+        }
+	}
+}
+ 
+void read_Mnist_Images(string filename, MatrixXf&images, const vector<int> idx) {
+	std::ifstream file(filename, std::ios::binary);
+	if (file.is_open())
+	{
+		int magic_number = 0;
+		int number_of_images = 0;
+		int n_rows = 0;
+		int n_cols = 0;
+		unsigned char label;
+		file.read((char*)&magic_number, sizeof(magic_number));
+		file.read((char*)&number_of_images, sizeof(number_of_images));
+		file.read((char*)&n_rows, sizeof(n_rows));
+		file.read((char*)&n_cols, sizeof(n_cols));
+		magic_number = ReverseInt(magic_number);
+		number_of_images = ReverseInt(number_of_images);
+		n_rows = ReverseInt(n_rows);
+		n_cols = ReverseInt(n_cols);
+ 
+		cout << "magic number = " << magic_number << endl;
+		cout << "number of images = " << number_of_images << endl;
+		cout << "rows = " << n_rows << endl;
+		cout << "cols = " << n_cols << endl;
+        
+        int n = 0;
+        images.resize(idx.size(), n_rows*n_cols);
+		for (int i = 0; i < number_of_images; i++)
+		{   
+			for (int r = 0; r < n_rows; r++)
+			{
+				for (int c = 0; c < n_cols; c++)
+				{
+					unsigned char image = 0;
+					file.read((char*)&image, sizeof(image));
+                    if(i == idx[n]) images(n, r*n_cols+c) = image;
+				}
+			}
+            if(i == idx[n]) ++n;
+		}
+	}
+}
+ 
+// int main()
+// {
+// 	/*
+// 	vector<double>labels;
+// 	read_Mnist_Label("t10k-labels.idx1-ubyte", labels);
+// 	for (auto iter = labels.begin(); iter != labels.end(); iter++)
+// 	{
+// 		cout << *iter << " ";
+// 	}
+// 	*/
+// 	vector<vector<double>>images;
+// 	read_Mnist_Images("t10k-images.idx3-ubyte", images);
+// 	for (int i = 0; i < images.size(); i++)
+// 	{
+// 		for (int j = 0; j < images[0].size(); j++)
+// 		{
+// 			cout << images[i][j] << " ";
+// 		}
+// 	}
+// 	return 0;
+// }
